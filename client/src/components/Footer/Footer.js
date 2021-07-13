cc311a20419091c18d369a751491c3cf460cd92c
Divya <agarrwaldivya3009@gmail.com>
2021-07-13 22:12:32 +0530

    done

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -32,7 +32,7 @@ function Example() {
           <ContextAwareToggle eventKey="0">User Guide</ContextAwareToggle>
         </Card.Header>
         <Accordion.Collapse eventKey="0">
-          <Card.Body>When you enter your name you can copy the code through "copy code" and then share it with your friend. Then, your friend will write his name and paste the provided code in "Make a call". Then, press the call button and you finally, both the connected to have a video conversation with each other</Card.Body>
+          <Card.Body>When you enter your name a code is generated which you can copy the code through "copy code" and then share it with your friend via whats App, faceook and twitter. Then, your friend will enter his name and paste the provided code in "Make a call" option. Then, press the "call" button and then both the person are connected to have a video conversation with each other</Card.Body>
         </Accordion.Collapse>
       </Card>
     </Accordion>
